663237a4e5b112d96971daad3d627661a30e09af
ankit <devaniankit1990@gmail.com>
2022-02-06 23:38:42 +0530

    modified home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,19 @@
 import "./Home.css"
 import Me from "./images/Me.jpg"
 const Home = () => {
-   return <>
+   return( <>
         <section id="header" className ="">
             <div className = "container-fluid nav_bg">
                 <div className = "row">
                     <div className = "col-10 mx-auto">
-                        <div className ="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                        <div className ="col-md-6">
                             <h1 className="intro">
                                 Welcome to TMC
                             </h1>
                             <h2 className = "tagline"> Multitasking is new trend, We nurture it</h2>
                             <p className = "desc">We conduct games, simulation based leagues, webinars, seminars, talk-shows, conclaves and various other interesting activities. </p>
                         </div>
-                        <div className ="col-md-6 order-1 order-lg-2 logo"> 
+                        <div className ="col-md-6"> 
                         <img src ={Me} className ="img-fluid"/>
                         </div>
                     </div>
@@ -21,7 +21,7 @@ const Home = () => {
             </div>
         </section>
     </>
-}
+   )}
 
-export default Home
+export default Home;
 
